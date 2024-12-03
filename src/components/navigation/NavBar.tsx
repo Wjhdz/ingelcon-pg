6f4c79c137e525482f2b5cf707f313e63a9923d6
Wjhdz <yorkjahf@gmail.com>
2024-12-03 11:25:31 -0500

    update nav component

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -10,12 +10,12 @@ import {
   Typography,
 } from "../../tools/desing";
 import { Colors } from "../../../src/utils/colors";
-import { DividerC as Divider } from "../antd/DividerC";
+import { DividerC } from "../antd/DividerC";
 import { MenuFoldOutlined } from "../../tools/icon";
 import Routers from "../../config/Router";
-import SvgIcon from "../../assests/Logo.svg";
+import SvgIcon from "../../assets/Logo.svg";
 import { InfoCompany } from "../../constants";
-import { colorSecondary as styleColorS } from "../../css/styles";
+
 const { nameCompany } = InfoCompany;
 const { secondary } = Colors;
 const { Text } = Typography;
@@ -26,45 +26,57 @@ const NavBar: React.FC = () => {
 
   return (
     <Row align="middle" style={{ minWidth: "330px" }}>
-      <Col xs={12} md={12} lg={4} xl={4} style={{ color: `${secondary}` }}>
+      <Col
+        xs={12}
+        sm={12}
+        md={12}
+        lg={4}
+        xl={4}
+        style={{ color: `${secondary}` }}
+      >
         <Avatar
           src={<img src={SvgIcon} onClick={() => navigate("/")} />}
           alt="Custom Icon"
         ></Avatar>
-        <Divider />
+        <DividerC
+          style={{ background: `${secondary}`, color: `${secondary}` }}
+          type={"vertical"}
+        />
         <Button
           type="link"
           onClick={() => navigate("/")}
           style={{ paddingInlineStart: "2px", color: `${secondary}` }}
         >
           <Text style={{ color: secondary }}>
-            {nameCompany.split(" S.A.S.")[0]}
+            {nameCompany.split(" S.A.S.")}
           </Text>
         </Button>
       </Col>
       <Col
         xs={12}
+        sm={12}
         md={12}
         lg={0}
         xl={0}
         style={{ textAlign: "end", color: `${secondary}` }}
       >
         <Button type="link" onClick={() => setStateOpen(true)}>
-          <MenuFoldOutlined style={styleColorS} />
+          <MenuFoldOutlined style={{ color: secondary }} />
         </Button>
       </Col>
-      {Routers().map((router) => (
+      {Routers().map((router, index) => (
         <>
           {router.visible === true && (
             <Col
               xs={0}
+              sm={0}
               md={0}
               lg={4}
               xl={4}
-              xxl={4}
               style={{ textAlign: "end" }}
             >
               <Button
+                key={index}
                 type="link"
                 onClick={() => navigate(router.path)}
                 style={{ color: `${secondary}` }}
@@ -84,8 +96,11 @@ const NavBar: React.FC = () => {
                 onClick={() => navigate("/")}
                 alt="Custom Icon"
               ></Avatar>
-              <Divider />
-              {nameCompany.split(" S.A.S.")[0]}
+              <DividerC
+                style={{ background: `${secondary}`, color: `${secondary}` }}
+                type={"vertical"}
+              />
+              {nameCompany.split(" S.A.S.")}
             </Col>
           </Row>
         }
@@ -96,14 +111,15 @@ const NavBar: React.FC = () => {
         width={"280px"}
       >
         <Row gutter={[8, 8]}>
-          {Routers().map((router) => (
+          {Routers().map((router, index) => (
             <>
               {router.visible === true && router.path != "" && (
                 <Col span={24}>
                   <Button
+                    key={index}
                     type="link"
                     onClick={() => navigate(router.path)}
-                    style={styleColorS}
+                    style={{ color: secondary }}
                   >
                     {router.name}
                   </Button>
--- src/css/styles.ts
+++ src/css/styles.ts
@@ -1,35 +1,11 @@
 import { Colors } from "../utils/colors";
 const { secondary, white } = Colors;
 
-export const colorSecondary = { color: `${secondary}` };
-
-export const colorWhite = { color: `${white}` };
-
-export const backgroundColorWhite = { background: `${white}` };
-
-export const colorSecondaryAndWhite = {
-  ...colorWhite,
-  background: `${secondary}`,
-};
-
-export const card = {
-  padding: "10px",
-  margin: "10px",
-  borderRadius: "10px",
-  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
-};
-
-export const rows = {
-  ...card,
-  display: "flex",
-};
-
 export const headerLayoutStyle = {
   minWidth: "412px ",
   position: "sticky" as "sticky",
   top: 0,
   zIndex: 1,
-  padding: "0px 25px  ",
   width: "100%",
   background: white,
   borderBottom: `2px solid ${secondary}`,
@@ -47,5 +23,5 @@ export const footerLayoutStyle = {
   padding: "20px",
   borderRadius: "1em",
   minWidth: "411px",
-  ...backgroundColorWhite,
+  backgroundcolor: white,
 };
